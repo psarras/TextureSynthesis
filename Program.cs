be598134f18888615d413fd24695d8384f889763
spsarras <psarras.st@gmail.com>
2019-10-24 17:00:11 +0100

    moved classes to separate files and kept main function clean

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TextureSynthesisConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            TextureSynthesis.RunAllExamples();
+        }
+    }
+}
